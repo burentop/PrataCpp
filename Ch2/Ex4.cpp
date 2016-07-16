1f7335e333ae636ff254081ca12609a91e6bb18f
Brent Perry <burentoperii@gmail.com>
2016-07-16 15:47:53 -0500

    Add Exercise 4

---
+++ Ch2/Ex4.cpp
@@ -0,0 +1,11 @@
+// Ex4.cpp -- converts age in years to months
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int years;
+    cout << "Enter your age in years: ";
+    cin >> years;
+    cout << "Your age in months is " << years * 12 << endl;
+}
